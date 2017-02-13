ba979efeda6f4f524bdae456e2184d5398a2944e
Andrzej <angan@o2.pl>
2017-02-13 11:17:48 +0100

    Dziala

--- latwe/flamaster.c
+++ latwe/flamaster.c
@@ -1,26 +1,38 @@
 #include <stdio.h>
 
+int ile_powtorzen(char *);
 
 int main(void)
 {
-	int t;
+	int t, s;
 	char tab[200];
-	char *wsk, *wsk_nastepny, wsk_plus_dwa;
+	char *wsk1;
 	scanf("%d", &t);
 	while(t--)
 	{
 		scanf("%s", tab);
-		wsk = tab;
-		wsk_nastepny = wsk + 1;
-		wsk_plus_dwa = wsk +2;
-		if( *wsk != *wsk_nastepny )
+		wsk1 = &tab[0];
+		while(*wsk1 != '\0')
 		{
-			printf("%c\n", *wsk);
-			wsk ++;
+			s = ile_powtorzen(wsk1);
+			if( s == 1 ) printf("%c", *wsk1);
+			if( s == 2 ) printf("%c%c", *wsk1, *wsk1);
+			if( s  > 2 ) printf("%c%d", *wsk1, s);
+			wsk1 = wsk1 + s;
 		}
-		printf("%c\n", *wsk);
-
+		printf("\n");
 	}
-
 	return 0;
+}
+
+
+int ile_powtorzen(char *wsk)
+{
+	int s = 1;
+	while( (*(wsk + 1) != '\0') && (*wsk == *(wsk + 1)) )
+	{
+		s++;
+		wsk++;
+	}
+	return s;
 }

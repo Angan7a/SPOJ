844dafc10fe78d654c303294ab4ad5ce4b597882
Andrzej <angan@o2.pl>
2017-02-22 12:18:49 +0100

    dziala Stefan

--- latwe/Stefan.c
+++ latwe/Stefan.c
@@ -1,30 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void)
 {
-    int ile, sum_left, sum_right, sum_l_max, sum_r_max, sum_max = 0;
-    int *tab;
+    int ile, a;
+    long long sum = 0, sum_max = 0;
     scanf("%d", &ile);
-    tab = (int *)malloc(ile * sizeof(*tab));
-    for  (int i = 0; i <  ile; i++) {
-        scanf("%d", tab + i);
-    }
-    for (int d = 0; d <  ile; d++) {
-        sum_left = sum_right = sum_l_max = sum_r_max = 0;
-        for (int i = d -1; i >= 0; i--) {
-            sum_left += *(tab + i);
-            if (sum_left > sum_l_max) sum_l_max = sum_left;
+    while (ile --) {
+        scanf("%d", &a);
+        if (sum > 0) {
+            sum += a;
+        } else {
+            sum = a;
         }
-        for (int i = d; i <  ile; i++) {
-            sum_right += *(tab + i);
-            if (sum_right > sum_r_max) sum_r_max = sum_right;
+        if (sum > sum_max) {
+            sum_max = sum;
         }
-        if (sum_r_max > sum_max) sum_max = sum_r_max;
-        if (sum_l_max > sum_max) sum_max = sum_l_max;
-        if (sum_l_max + sum_r_max > sum_max) sum_max = sum_l_max + sum_r_max;
-
     }
-    printf("%d\n", sum_max);
+    printf("%llu\n", sum_max);
     return 0;
 }

0c442ef05986a657632cc4b9aa4d1ee288c8b15b
Andrzej <angan@o2.pl>
2017-03-01 14:56:30 +0100

    krótszy kod

--- srednie/dodawanie_rzymskie.c
+++ srednie/dodawanie_rzymskie.c
@@ -10,8 +10,8 @@ na_rzymskie(int);
 int main(void)
 {
     char a[20], b[20], dl_a, dl_b;
-    int aa[20], bb[20], sum_a, sum_b, sum;
-    aa[0] = bb[0] = 1000;
+    int aa[20], bb[20], sum, z;
+    aa[0] = bb[0] = 2000;
     while (scanf("%s %s", a, b) != EOF) {
         dl_a = 0;
         dl_b = 0;
@@ -29,136 +29,55 @@ int main(void)
                 bb[i] *= -1;
             }
         }
-        sum_a = sum_b = 0;
+        sum = 0;
         for (int i = 1; i <= dl_a; i++) {
-            sum_a += aa[i];
+            sum += aa[i];
         }
 
         for (int i = 1; i <= dl_b; i++) {
-            sum_b += bb[i];
+            sum += bb[i];
         }
-//        printf("%d %d\n", sum_a, sum_b);
-        sum = sum_a + sum_b;
-
-
-        if (sum/2000 == 1) {
-            printf("%c%c", na_rzymskie(1000), na_rzymskie(1000));
-            sum = sum % 2000;
-        }
-        if (sum/1000 == 1) {
-            printf("%c", na_rzymskie(1000));
-            sum = sum % 1000;
-        }
-        if (sum/900 == 1) {
-            printf("%c%c", na_rzymskie(100), na_rzymskie(1000));
-            sum = sum % 900;
-        }
-        if (sum/800 == 1) {
-            printf("%c%c%c%c", na_rzymskie(500), na_rzymskie(100), na_rzymskie(100), na_rzymskie(100));
-            sum = sum % 800;
-        }
-        if (sum/700 == 1) {
-            printf("%c%c%c", na_rzymskie(500), na_rzymskie(100), na_rzymskie(100));
-            sum = sum % 700;
-        }
-        if (sum/600 == 1) {
-            printf("%c%c", na_rzymskie(500), na_rzymskie(100));
-            sum = sum % 600;
-        }
-        if (sum/500 == 1) {
-            printf("%c", na_rzymskie(500));
-            sum = sum % 500;
-        }
-        if (sum/400 == 1) {
-            printf("%c%c", na_rzymskie(100), na_rzymskie(500));
-            sum = sum % 400;
-        }
-        if (sum/300 == 1) {
-            printf("%c%c%c", na_rzymskie(100), na_rzymskie(100), na_rzymskie(100));
-            sum = sum % 300;
-        }
-        if (sum/200 == 1) {
-            printf("%c%c", na_rzymskie(100), na_rzymskie(100));
-            sum = sum % 200;
-        }
-        if (sum/100 == 1) {
-            printf("%c", na_rzymskie(100));
-            sum = sum % 100;
-        }
-        if (sum/90 == 1) {
-            printf("%c%c", na_rzymskie(10), na_rzymskie(100));
-            sum = sum % 90;
-        }
-        if (sum/80 == 1) {
-            printf("%c%c%c%c", na_rzymskie(50), na_rzymskie(10), na_rzymskie(10), na_rzymskie(10));
-            sum = sum % 80;
-        }
-        if (sum/70 == 1) {
-            printf("%c%c%c", na_rzymskie(50), na_rzymskie(10), na_rzymskie(10));
-            sum = sum % 70;
-        }
-        if (sum/60 == 1) {
-            printf("%c%c", na_rzymskie(50), na_rzymskie(10));
-            sum = sum % 60;
-        }
-        if (sum/50 == 1) {
-            printf("%c", na_rzymskie(50));
-            sum = sum % 50;
-        }
-        if (sum/40 == 1) {
-            printf("%c%c", na_rzymskie(10), na_rzymskie(50));
-            sum = sum % 40;
-        }
-        if (sum/30 == 1) {
-            printf("%c%c%c", na_rzymskie(10), na_rzymskie(10), na_rzymskie(10));
-            sum = sum % 30;
-        }
-        if (sum/20 == 1) {
-            printf("%c%c", na_rzymskie(10), na_rzymskie(10));
-            sum = sum % 20;
-        }
-        if (sum/10 == 1) {
-            printf("%c", na_rzymskie(10));
-            sum = sum % 10;
-        }
-        if (sum/9  == 1) {
-            printf("%c%c", na_rzymskie(1), na_rzymskie(10));
-            sum = sum % 9;
-        }
-        if (sum/8 == 1) {
-            printf("%c%c%c%c", na_rzymskie(5), na_rzymskie(1), na_rzymskie(1), na_rzymskie(1));
-            sum = sum % 8;
-        }
-        if (sum/7 == 1) {
-            printf("%c%c%c", na_rzymskie(5), na_rzymskie(1), na_rzymskie(1));
-            sum = sum % 7;
-        }
-        if (sum/6 == 1) {
-            printf("%c%c", na_rzymskie(5), na_rzymskie(1));
-            sum = sum % 6;
-        }
-        if (sum/5 == 1) {
-            printf("%c", na_rzymskie(5));
-            sum = sum % 5;
-        }
-        if (sum/4 == 1) {
-            printf("%c%c", na_rzymskie(1), na_rzymskie(5));
-            sum = sum % 4;
-        }
-        if (sum/3 == 1) {
-            printf("%c%c%c", na_rzymskie(1), na_rzymskie(1), na_rzymskie(1));
-            sum = sum % 3;
-        }
-        if (sum/2 == 1) {
-            printf("%c%c", na_rzymskie(1), na_rzymskie(1));
-            sum = sum % 2;
-        }
-        if (sum/1 == 1) {
-            printf("%c", na_rzymskie(1));
-            sum = sum % 1;
+        z = 10000;
+        while (sum) {
+           if ((sum / (9 * z)) == 1) {
+               printf("%c%c", na_rzymskie(z), na_rzymskie(z * 10));
+     	       sum = sum % (9*z);
+           }
+           if ((sum / (8 * z)) == 1) {
+               printf("%c%c%c%c", na_rzymskie(5*z), na_rzymskie(z), na_rzymskie(z), na_rzymskie(z));
+               sum = sum % (8*z);
+           }
+           if ((sum / (7 * z)) == 1) {
+               printf("%c%c%c", na_rzymskie(5*z), na_rzymskie(z), na_rzymskie(z));
+               sum = sum % (7*z);
+           }
+           if ((sum / (6 * z)) == 1) {
+               printf("%c%c", na_rzymskie(5*z), na_rzymskie(z));
+               sum = sum % (6*z);
+           }
+           if ((sum / (5 * z)) == 1) {
+               printf("%c", na_rzymskie(5*z));
+               sum = sum % (5*z);
+           }
+           if ((sum / (4 * z)) == 1) {
+               printf("%c%c", na_rzymskie(z), na_rzymskie(5*z));
+               sum = sum % (4*z);
+           }
+           if ((sum / (3 * z)) == 1) {
+               printf("%c%c%c", na_rzymskie(z), na_rzymskie(z), na_rzymskie(z));
+               sum = sum % (3*z);
+           }
+           if ((sum / (2 * z)) == 1) {
+               printf("%c%c", na_rzymskie(z), na_rzymskie(z));
+               sum = sum % (2*z);
+           }
+           if ((sum / (1 * z)) == 1) {
+               printf("%c", na_rzymskie(z));
+               sum = sum % (1*z);
+           }
+           z /= 10;
         }
         printf("\n");
-
     }
     return 0;
 }
